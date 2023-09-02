e8ee57eba5b6d415118b3c42beaea8b09c2ae5b5
Nataliia Andrieieva <natalia__forever@ukr.net>
2023-09-02 16:48:06 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Nataliia!");
+Console.WriteLine("Goodbye");
+//Method that would be run
+Console.ReadLine();
